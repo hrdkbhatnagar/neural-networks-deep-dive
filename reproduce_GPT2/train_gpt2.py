36faafba607685dde9ad07e834149d1acea45b77
Hardik Bhatnagar <hrdk.bhatnagar@gmail.com>
2024-06-11 17:08:41 +0000

    create a dataloader (shakespeare)

--- reproduce_GPT2/train_gpt2.py
+++ reproduce_GPT2/train_gpt2.py
@@ -176,21 +176,38 @@ max_length = 30
 
 
 device = 'cuda'
-# prefix tokens 
+
+
 import tiktoken
-enc = tiktoken.get_encoding('gpt2')
 
-with open('/datadrive1/hbhatnagar/home/fun/neural-networks-deep-dive/nanoGPT/datasets/tiny-shakespeare/input.txt', 'r') as f:
-    text = f.read()
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
 
-text = text[:1000]
-tokens = enc.encode(text)
-B,T = 4, 32 
+        # at init load tokens from disk and store them in memory
+        with open('/datadrive1/hbhatnagar/home/fun/neural-networks-deep-dive/nanoGPT/datasets/tiny-shakespeare/input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
 
-buf = torch.tensor(tokens[:B*T +1])
-buf = buf.to(device)
-x = buf[:-1].view(B,T)
-y = buf[1:].view(B,T)
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
 
 
 # Get logits 
@@ -198,11 +215,14 @@ y = buf[1:].view(B,T)
 model = GPT(GPTConfig())
 # model.eval()
 model.to(device)
-logits, loss  = model(x, y)
+
 
 optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
+train_loader = DataLoaderLite(B = 4, T = 32)
 
 for i in range(50):
+    x,y = train_loader.next_batch()
+    x,y  = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x,y)
     loss.backward()
