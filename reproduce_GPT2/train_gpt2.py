59201830c85fdef7a99d27e3a2c1d849aab41efa
Hardik Bhatnagar <hrdk.bhatnagar@gmail.com>
2024-06-11 16:54:06 +0000

    overfit on single batch

--- reproduce_GPT2/train_gpt2.py
+++ reproduce_GPT2/train_gpt2.py
@@ -99,7 +99,7 @@ class GPT(nn.Module):
         ))
         self.lm_head  = nn.Linear(config.n_embd, config.vocab_size, bias = False)
 
-    def forward(self, idx):
+    def forward(self, idx, targets = None):
         # idx is of shape (B, T)
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
@@ -114,7 +114,10 @@ class GPT(nn.Module):
         # forward the final layernorm and the classifier
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
-        return logits
+        loss = None 
+        if targets is not None: 
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss 
 
     @classmethod
     def from_pretrained(cls, model_type):
@@ -171,13 +174,46 @@ class GPT(nn.Module):
 num_return_sequences = 5 
 max_length = 30 
 
-model = GPT.from_pretrained('gpt2')
-model.eval()
-model.to('cuda')
 
+device = 'cuda'
 # prefix tokens 
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
+
+with open('/datadrive1/hbhatnagar/home/fun/neural-networks-deep-dive/nanoGPT/datasets/tiny-shakespeare/input.txt', 'r') as f:
+    text = f.read()
+
+text = text[:1000]
+tokens = enc.encode(text)
+B,T = 4, 32 
+
+buf = torch.tensor(tokens[:B*T +1])
+buf = buf.to(device)
+x = buf[:-1].view(B,T)
+y = buf[1:].view(B,T)
+
+
+# Get logits 
+# model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
+# model.eval()
+model.to(device)
+logits, loss  = model(x, y)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
+
+for i in range(50):
+    optimizer.zero_grad()
+    logits, loss = model(x,y)
+    loss.backward()
+    optimizer.step()
+    print(f'step {i}, loss: {loss.item()}')
+
+
+
+
+import sys; sys.exit(0)
+
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype = torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
